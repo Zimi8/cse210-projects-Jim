ac4a85f55203e420aa1a38b5599998d42a24f4b1
EzequielJimenez <ezequielemilianojimenez@gmail.com>
2026-03-11 10:39:55 -0300

    third test activity

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string response;
+
+        do
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
