45146ed78ccf7f4d1f17c9bfbd195940b0bdb6c9
EzequielJimenez <ezequielemilianojimenez@gmail.com>
2026-03-11 09:03:59 -0300

    first activity1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your Last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {name} and your last name is {lastName}.");
+    
     }
 }
